77be329a61f483e2b42760cbabbc97c37ea97740
Likemilk <dydwls121200@gmail.com>
2016-12-31 06:53:52 +0000

    hello

---
+++ server/venv/push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#echo "This Script Executable File : $0"
+#echo "Argument Count : $#"
+#echo "Argument List \$* : $*"
+#echo "Argument List \$@ : $@"
+#echo "Argument 1 : $1"
+#echo "Argument 2 : $2"
+#echo "Argument 3 : $3"
+#echo "Argument 4 : $4"
+git status
+git add .
+git status
+git commit -m "$1"
+git push origin master
+
+
+
