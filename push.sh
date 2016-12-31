85335ee49fc1ba78b4fd199ba2d64f6433f03a0b
Likemilk <dydwls121200@gmail.com>
2016-12-31 04:03:28 +0000

    simple commit

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+git config --global user.email dydwls121200@gmail.com
+git config --global user.name Likemilk
+git add --all .
+git commit -m 'simple commit'
+git push origin master
+
+
+
+
+
+
---
+++ server/venv/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+source ./bin/activate
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000
+
+
+#Admin
+# User : DrakeJ
+# password : choco2323!
+
